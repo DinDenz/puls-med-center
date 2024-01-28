8d3b33e798fd0d149b5fde3ad6bd6b919be902ed
DinDenz <DinDenz@users.noreply.github.com>
2024-01-28 20:39:26 +0300

    fixed errors in slider, made ref in input phone

--- src/Components/Main/SectionForm/InputPhone.jsx
+++ src/Components/Main/SectionForm/InputPhone.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useRef} from 'react';
 
 export default function InputPhone() {
+    const phoneRef = useRef();
+    console.log(phoneRef);
+
     return (
-        <input className='form-elem' type="tel" name='tel'
-            placeholder='+375 (__) ___ - __ -__' required />
+        <input
+            className='form-elem phone-inpt'
+            ref={phoneRef}
+            type="tel"
+            name='tel'
+            placeholder='+375 (__) ___ - __ -__'
+            value=""
+            required />
     )
 }
--- src/Components/Main/SectionForm/SectionForm.jsx
+++ src/Components/Main/SectionForm/SectionForm.jsx
@@ -15,7 +15,7 @@ export default function SectionForm() {
             <form className='feedback-form' action="#" method="post">
               <div className='form-elem-containter'>
                 <select id="select" className='form-elem'>
-                  <option value selected disabled>Направления</option>
+                  <option value = "Направления" disabled>Направления</option>
                   <option value="Кардиология">Кардиология</option>
                   <option value="Детская кардиология">Детская кардиология</option>
                   <option value="Беременным">Беременным</option>
