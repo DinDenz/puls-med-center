22b93285766c437fc6719e597c4bd5b6154ec44a
DinDenz <DinDenz@users.noreply.github.com>
2024-01-28 18:25:05 +0300

    add base in section for form

--- src/Components/Main/SectionForm/SectionForm.jsx
+++ src/Components/Main/SectionForm/SectionForm.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 export default function SectionForm() {
   return (
-    <div>
-      
+    <div className='section-form'>
+      <div className='section-form-content'>
+        <div className='section-form__head head'>
+          <div className="head__title font-roboto-bold">Оставить заявку</div>
+          <div className="head__subtitle">Заполните форму ниже (все поля обязательны) и
+            мы свяжемся с Вами в течение 15 минут</div>
+        </div>
+        <div className='section-form__body'>
+          <div className="body__content">
+            <form className='feedback-form' action="#" method="post">
+              <div className='form-elem-containter'>
+                <select id="select" className='form-elem'>
+                  <option value selected disabled>Направления</option>
+                  <option value="Кардиология">Кардиология</option>
+                  <option value="Детская кардиология">Детская кардиология</option>
+                  <option value="Беременным">Беременным</option>
+                  <option value="Ревматология">Ревматология</option>
+                  <option value="Диагностика">Диагностика</option>
+                  <option value="Лабораторная диагностика">Лабораторная диагностика</option>
+                </select>
+              </div>
+              <div className='form-elem-containter inpt'><input className='form-elem inpt' type="text" name='FIO' placeholder='Ваши ФИО' required /></div>
+              <div className='form-elem-containter inpt'><input className='form-elem inpt' type="tel" name='tel' placeholder='+375 (__) ___ - __ -__' required /></div>
+              <div className='form-elem-containter'><input className='form-elem button' type="submit" name='submit' value='Отправить' /></div>
+            </form>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
