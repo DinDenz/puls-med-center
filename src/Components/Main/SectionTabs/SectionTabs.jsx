5fe3b0c711b3b3f7917f21424ee9c2820ddc36e0
DinDenz <DinDenz@users.noreply.github.com>
2024-02-07 14:04:47 +0300

    new section

--- src/Components/Main/SectionTabs/SectionTabs.jsx
+++ src/Components/Main/SectionTabs/SectionTabs.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import TabsNavigation from './TabsNavigation';
 
 export default function SectionTabs() {
   return (
-    <div>
-      
+    <div className='section-tabs'>
+      <div className="section-tabs__content">
+        <div className="tabs-head">
+          <div className="tabs-head__title font-roboto-bold">Наши услуги</div>
+        </div>
+        <div className="tabs-body">
+          <div className="tabs-body__nav"><TabsNavigation />component</div>
+          <div className="tabs-body__items">component</div>
+          stias eum alias molestiae praesentium libero obcaecati in eveniet.
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/Components/Main/SectionTabs/TabsNavigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TabsNavigation() {
+    return (
+        <nav>
+            <ul className="nav-list">
+                <li><a href="#">Направления</a></li>
+                <li><a href="#">Анализы</a></li>
+                <li><a href="#">Диагностика</a></li>
+            </ul>
+        </nav>
+    )
+}
