ec98974bb445915645d01f38658d2609261f4e13
DinDenz <DinDenz@users.noreply.github.com>
2024-02-09 12:53:04 +0300

    begin

--- src/Components/Main/SectionDifferents/SectionDifferents.jsx
+++ src/Components/Main/SectionDifferents/SectionDifferents.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Questions from './Questions';
 
 export default function SectionDifferents() {
   return (
     <div className='section-diffrents'>
-
+      <div className="section-diffrents__content">
+        <div className="diff-head">
+          <div className="diff-head__title">
+            Часто задаваемые вопросы
+          </div>
+        </div>
+        <div className="diff-body">
+          <div className="diff-body__items">
+            <Questions />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
