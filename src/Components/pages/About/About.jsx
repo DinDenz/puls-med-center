a6b12989b8a6f9756a5973c18ddbe1d888a4dce1
DinDenz <DinDenz@users.noreply.github.com>
2024-02-15 19:47:22 +0300

    base of about

---
+++ src/Components/pages/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='main'>
+            <div className='container' >
+                <div className="main__content">
+                    <p className='egege'>вышел заяц на крыльцо почесать свое яйцо</p>
+                    <SectionForm />
+                </div>
+            </div>
+        </div>
+  )
+}
