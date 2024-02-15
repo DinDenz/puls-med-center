8ef2051723686f1db867f13d4fcaf7d32cd2bb61
DinDenz <DinDenz@users.noreply.github.com>
2024-02-15 19:32:59 +0300

    base of components

---
+++ src/Components/pages/Otzivy/OtzivyComponents/FormForRew.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FormForRew() {
+  return (
+    <div>
+        <h1 className='olala'>это компонент для формы</h1>
+    </div>
+  )
+}
---
+++ src/Components/pages/Otzivy/OtzivyComponents/SectionRewList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SectionRewList() {
+  return (
+    <div>
+      <h1 className="igogo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni laboriosam minus, tempora dolore quidem corrupti autem itaque nostrum repellat, recusandae temporibus sapiente porro blanditiis quod? Omnis beatae exercitationem cumque veritatis.</h1>
+    </div>
+  )
+}
