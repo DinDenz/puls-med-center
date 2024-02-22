8e051dacef9490908d93b5799d9da0e77fb91419
DinDenz <DinDenz@users.noreply.github.com>
2024-02-22 20:18:37 +0300

    first

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Logo(props) {
     return (
         <div className='logo-box'>
-            <a href='/puls-med-center'><img className="logo" src={props.image} width={props.width} height={props.height} alt="Logo" /></a>
+            <a href='*'><img className="logo" src={props.image} width={props.width} height={props.height} alt="Logo" /></a>
         </div>
 
     )
