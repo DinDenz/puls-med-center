f764f6bab44e8a0a371060040691a8bcecda870a
DinDenz <DinDenz@users.noreply.github.com>
2024-02-28 19:02:13 +0300

    map reviews using rtk

--- src/store/reviewsSlice.js
+++ src/store/reviewsSlice.js
@@ -3,7 +3,32 @@ import { createSlice } from "@reduxjs/toolkit";
 const reviewsSlice = createSlice({
     name: 'reviews',
     initialState: {
-        reviews: []
+        reviews: [
+            {
+                id: 1,
+                name: "Надежда",
+                text: "Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове.",
+                theme: "Благодарность",
+            },
+            {
+                id: 2,
+                name: "Антон",
+                text: "Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60.",
+                theme: "Благодарность",
+            },
+            {
+                id: 3,
+                name: "Игорь",
+                text: "Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60.",
+                theme: "Благодарность",
+            },
+            {
+                id: 4,
+                name: "Анна",
+                text: "Благодаря этой клинике я смогла избавиться от давней проблемы с зубами. Лечение было безболезненным и эффективным, а атмосфера в клинике создает уют и комфорт. Очень довольна результатом!",
+                theme: "Благодарность",
+            },
+        ]
     },
     reducers: {
         addRev(state, action) {
@@ -13,6 +38,7 @@ const reviewsSlice = createSlice({
                 id: new Date().toISOString(),
                 userName: action.payload.userName,
                 text: action.payload.text,
+                theme: action.payload.theme,
 
             })
         },
@@ -20,5 +46,5 @@ const reviewsSlice = createSlice({
     }
 })
 
-export const {addRev, deleteRev} = reviewsSlice.actions;
+export const { addRev, deleteRev } = reviewsSlice.actions;
 export default reviewsSlice.reducer;
