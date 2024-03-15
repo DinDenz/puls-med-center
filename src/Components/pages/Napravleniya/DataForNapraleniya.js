80cbeeaa250842fd9b686cb3d62f19cbb2d6e577
DinDenz <DinDenz@users.noreply.github.com>
2024-03-15 15:57:58 +0300

    add elements

--- src/Components/pages/Napravleniya/DataForNapraleniya.js
+++ src/Components/pages/Napravleniya/DataForNapraleniya.js
@@ -4,8 +4,8 @@ const napravleniya = [
         "type": "cardiologiya",
         "title": "Кардиология",
         "text": "Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.",
-        "price-first": "50",
-        "price-second": "40",
+        "price_first": "50",
+        "price_second": "40",
         "howtobook": "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo",
     },
     {
@@ -13,8 +13,8 @@ const napravleniya = [
         "type": "detskaya-cardiologiya",
         "title": "Детская кардиология",
         "text": "Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.",
-        "price-first": "55",
-        "price-second": "45",
+        "price_first": "55",
+        "price_second": "45",
         "howtobook": "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo",
     },
     {
@@ -22,8 +22,8 @@ const napravleniya = [
         "type": "revmatologiya",
         "title": "Ревматология",
         "text": "Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.",
-        "price-first": "56",
-        "price-second": "45",
+        "price_first": "56",
+        "price_second": "45",
         "howtobook": "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo",
     },
     {
@@ -31,8 +31,8 @@ const napravleniya = [
         "type": "cardiologiya-beremennih",
         "title": "Беременным",
         "text": "Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.",
-        "price-first": "45",
-        "price-second": "40",
+        "price_first": "45",
+        "price_second": "40",
         "howtobook": "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo",
     },
 ]
--- src/Components/pages/Napravleniya/NapravleniyaPage.jsx
+++ src/Components/pages/Napravleniya/NapravleniyaPage.jsx
@@ -13,10 +13,50 @@ export default function NapravleniyaPage() {
       <div className='main'>
         <div className='container' >
           <div className="main__content">
+            <div className='napr-title-section'>
+              <div className="back">
+                <div className="back-background"></div>
+              </div>
+              <h1>{currentNapravl.title}</h1>
+            </div>
             {currentNapravl ? (
-              <div className="doc-page" key={currentNapravl.id}>
-                {currentNapravl.title}
-                {currentNapravl.text}
+              <div className="napr-page" key={currentNapravl.id}>
+                <div className="napr-page__text">{currentNapravl.text}</div>
+                <div className="napr-page__table">
+                  <p className="napr-page__table_info">
+                    Стоимость консультации врача
+                  </p>
+                  <table className="table">
+                    <thead className="table__head">
+                      <tr className="table__head_row">
+                        <th className="table__head_row--title">
+                          Услуга
+                        </th>
+                        <th className="table__head_row--cost">
+                          Стоимость
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="table__body">
+                      <tr className="table__body_row">
+                        <td>
+                          Первичный прием врача
+                        </td>
+                        <td>
+                          <span>{currentNapravl.price_first} BYN</span>
+                        </td>
+                      </tr>
+                      <tr className="table__body_row">
+                        <td>
+                          Повторный прием врача
+                        </td>
+                        <td>
+                          <span>{currentNapravl.price_second} BYN</span>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             ) : (
               <NotFound />
