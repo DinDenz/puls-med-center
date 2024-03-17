1b1da6b22d79dd4122d6f46dcea616417332c982
DinDenz <DinDenz@users.noreply.github.com>
2024-03-17 15:27:01 +0300

    update About

--- src/Components/pages/About/About.jsx
+++ src/Components/pages/About/About.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import SectionForm from '../../Main/SectionForm/SectionForm';
 import SectionTabs from '../../Main/SectionTabs/SectionTabs';
-import about_img from "./../../../IMG/about_pic.png";
+import dataForAbout from './dataForAbout'
 
 export default function About() {
-  const dataForAbout = [
-    {
-      id: "about-info",
-      text_1: "Adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun",
-      text_2: "Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu.",
-      text_3: `Это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.`,
-      text_4: "Комплексный подход к диагностике и лечению пациента, повышенные требования к качеству медицинской помощи и сервису, полное сопровождение пациента на всех этапах лечения, инновации, доступность и своевременность медицинской помощи — визитная карточка медицинского центра «Пульс».",
-      ul: {
-        '1': "своевременность и точность диагностики,",
-        "2": "успешность и качество лечения,",
-        "3": "этика и теплота общения,",
-        "4": "комфорт пребывания,"
-      },
-      img: about_img,
-    }
-  ]
+
   return (
     <div className='main'>
       <div className='container' >
@@ -58,7 +43,7 @@ export default function About() {
             </div>
 
           ))}
-          <SectionTabs/>
+          <SectionTabs />
           <SectionForm />
         </div>
       </div>
